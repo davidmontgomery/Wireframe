b83a5a444e7ff2736680c53ccac7b3739171d325
David Montgomery <hello@davidmontgomery.co.nz>
2011-08-31 08:16:24 +1200

    Tidy Gallery page code.
    Signed-off-by: David Montgomery <hello@davidmontgomery.co.nz>

--- mysite/code/page_types/GalleryPage.php
+++ mysite/code/page_types/GalleryPage.php
@@ -1,54 +1,52 @@
 <?php
 
-class GalleryPage extends Page {   
-    static $db = array(
-    );
-    
-    static $has_one = array(
-    );
-    
-    static $has_many = array(
-        'GalleryImages' => 'GalleryImage'
-    );
-    
-    function getCMSFields() {
-        $fields = parent::getCMSFields();
-          
-          $manager = new ImageDataObjectManager(
-              $this, // Controller
-              'GalleryImages', // Source name
-              'GalleryImage', // Source class
-              'MyGalleryImage', // File name on DataObject
-              array(
-                  'GalleryImageTitle' => 'GalleryImageTitle'
-              ), // Headings
+class GalleryPage extends Page {
 
-              'getCMSFields_forPopup' // Detail fields (function name or FieldSet object)
-              // Filter clause
-              // Sort clause
-              // Join clause
-          );
-          
-          $fields->addFieldToTab('Root.Content.GalleryImage', $manager);
-          
-          return $fields;
-    }
-}
+	static $db = array(
+	);
+
+	static $has_one = array(
+	);
+
+	static $has_many = array(
+		'GalleryImages' => 'GalleryImage'
+	);
+
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$manager = new ImageDataObjectManager(
+			$this,
+			'GalleryImages',
+			'GalleryImage',
+			'MyGalleryImage',
+				array(
+					'GalleryImageTitle' => 'GalleryImageTitle'
+				),
+			'getCMSFields_forPopup'
+		);
+
+		$fields->addFieldToTab('Root.Content.GalleryImage', $manager);
+			return $fields;
+		}
+	}
 
 class GalleryPage_Controller extends Page_Controller {
-    function GalleryPaginate() {
-        $numImages = 5; // Number of photos per page
-        
-        if(!isset($_GET['start']) || !is_numeric($_GET['start']) || (int)$_GET['start'] < 1) $_GET['start'] = 0;
-        $SQL_start = (int)$_GET['start'];
-        $doSet = DataObject::get(
-        $callerClass = "GalleryImage",
-        $filter = "`BelongToEventPageID` = '".$this->ID."'",
-        $sort = "",
-        $join = "",
-        $limit = "{$SQL_start}, $numImages"
-        );
+	function GalleryPaginate() {
+		$numImages = 5; // Number of photos per page
 
-        return $doSet ? $doSet : false;
-    }
+		if(!isset($_GET['start']) || !is_numeric($_GET['start']) || (int)$_GET['start'] < 1) $_GET['start'] = 0;
+
+		$SQL_start = (int)$_GET['start'];
+		$doSet = DataObject::get(
+			$callerClass = "GalleryImage",
+			$filter = "`BelongToEventPageID` = '".$this->ID."'",
+			$sort = "",
+			$join = "",
+			$limit = "{$SQL_start}, $numImages"
+		);
+
+		return $doSet ? $doSet : false;
+	}
+
 }
