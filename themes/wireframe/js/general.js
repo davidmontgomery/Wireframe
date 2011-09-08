46eeb87cf61171b69826a4ddb61e8a3f5c374c47
David Montgomery <hello@davidmontgomery.co.nz>
2011-09-09 08:10:38 +1200

    Minor changes to lightbox
    Signed-off-by: David Montgomery <hello@davidmontgomery.co.nz>

--- themes/wireframe/js/general.js
+++ themes/wireframe/js/general.js
@@ -17,20 +17,49 @@
 		});
 		
 		// Fancybox
-		$('#various1').attr('href', '#inline1'); // need this for inline box to work with SS	
-
+		$('#various1').attr('href', '#inline1'); // need this for inline box to work with SS
+		
 		$('#various1').fancybox({
-			'width' : 200,
-			'height' : 200,
 			'padding' : 20,
-			'titlePosition' : 'inside',
-			'transitionIn' : 'elastic',
-			'transitionOut':'elastic',
-			'speedIn' : 400,
-			'speedOut' : 300,
-			'opacity' : true,
+			'margin' : 40,
+			'opacity' : false,
+			'modal' : false,
+			'cyclic' : false,
+			'scrolling' : 'auto', // 'auto', 'yes' or 'no'
+			'width' : 560,
+			'height' : 340,
+			'autoScale' : true,
 			'autoDimensions' : false,
-			'centerOnScroll' : true
+			'centerOnScroll' : true,
+			'ajax' : {},
+			'swf' : { wmode: 'transparent' },
+			'hideOnOverlayClick' : true,
+			'hideOnContentClick' : false,
+			'overlayShow' : true,
+			'overlayOpacity' : 0.8,
+			'overlayColor' : '#000',
+			'titleShow' : true,
+			'titlePosition' : 'inside', // 'float', 'outside', 'inside' or 'over'
+			'titleFormat' : null,
+			'titleFromAlt' : false,
+			'transitionIn' : 'fade', // 'elastic', 'fade' or 'none'
+			'transitionOut' : 'fade', // 'elastic', 'fade' or 'none'
+			'speedIn' : 300,
+			'speedOut' : 300,
+			'changeSpeed' : 300,
+			'changeFade' : 'fast',
+			'easingIn' : 'swing',
+			'easingOut' : 'swing',
+			'showCloseButton' : true,
+			'showNavArrows' : true,
+			'enableEscapeButton' : true,
+			'enableKeyboardNav' : true,
+			onStart : function(){},
+			onCancel : function(){},
+			onComplete : function(){},
+			onCleanup : function(){},
+			onClosed : function(){},
+			onError : function(){}
 		});
 		
 	});
